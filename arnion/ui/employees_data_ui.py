519667fe3a9a81197527ab6aee5fce2cd7bb2e3d
Vladimir <vlad_e@rambler.ru>
2023-12-06 02:29:12 +0300

    Добавление формы записи  сотрудника

--- arnion/ui/employees_data_ui.py
+++ arnion/ui/employees_data_ui.py
@@ -1,5 +1,9 @@
+import copy
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox as mb
+
+from arnion.data.departments_data import DepartmentDataHandler
 from arnion.data.employees_data import EmployeeDataHandler, EmployeeDataObject
 
 
@@ -105,24 +109,35 @@ class EmployeesWindow:
 
     # Функция добавления записи
     def add_record(self):
-        pass
+        self.data_row = EmployeeDataObject()
+        self.record_window = EmployeeWindow(True, self.data_row, self)
+        self.record_window.open()
 
     # Функция завершения добавления записи
     def add_record_callback(self, added_data_row: EmployeeDataObject):
-        pass
+        EmployeeDataHandler.insert(added_data_row)
+        self.data_rows.append(added_data_row)
+        self.lbox_data_rows.insert('end', added_data_row.get_full_name())
+        self.lbox_data_rows.selection_clear(0, 'end')
+        self.lbox_data_rows.selection_set('end')
 
     # Функция редактирования записи
     def edit_record(self):
-        pass
+        self.selection = self.lbox_data_rows.curselection()[0]
+        self.data_row = copy.deepcopy(self.data_rows[self.selection])
+        self.record_window = EmployeeWindow(False, self.data_row, self)
+        self.record_window.open()
 
     # Функция завершения редактирования записи
     def edit_record_callback(self, edited_data_row: EmployeeDataObject):
-        pass
+        EmployeeDataHandler.update(edited_data_row)
+        self.data_rows[self.selection] = edited_data_row
+        self.refresh_listbox(self.selection, edited_data_row.get_full_name())
 
     # Функция удаления записи
     def delete_record(self):
         answer = mb.askokcancel(parent=self.window, title='Подтверждение',
-                             message='Вы действительно хотите удалить запись?')
+                                message='Вы действительно хотите удалить запись?')
         if not answer:
             return
         self.selection = self.lbox_data_rows.curselection()[0]
@@ -133,7 +148,9 @@ class EmployeesWindow:
 
     # Функция обновления списка
     def refresh_listbox(self, selection: int, value: str):
-        pass
+        self.lbox_data_rows.delete(selection, selection)
+        self.lbox_data_rows.insert(selection, value)
+        self.lbox_data_rows.select_set(selection)
 
     # Функция открытия окна
     def open(self):
@@ -145,3 +162,154 @@ class EmployeesWindow:
     # Функция закрытия окна
     def close(self):
         self.window.destroy()
+
+
+class EmployeeWindow:
+    # Конструктор
+    def __init__(self, add_new: bool, data_row: EmployeeDataObject, parent: EmployeesWindow):
+
+        if add_new:
+            title_text = 'Новый сотрудник'
+        else:
+            title_text = 'Редактирование сотрудника'
+
+        self.add_new = add_new
+        self.data_row = data_row
+        self.parent = parent
+
+        self.window = tk.Toplevel()
+        self.window.geometry('500x290')
+        self.window.title(title_text)
+        self.window.resizable(False, False)
+
+        # Добавление метки заголовка
+        lbl_title = tk.Label(self.window,
+                             text=title_text,
+                             font=('Helvetica', 16, 'bold'),
+                             fg='#0000cc',
+                             justify='center'
+                             )
+        lbl_title.place(x=25, y=15, width=450, height=50)
+
+        # Добавление полей ввода
+        lbl_first_name = tk.Label(self.window,
+                                  text='Имя:',
+                                  font=('Helvetica', 10, 'bold')
+                                  )
+        lbl_first_name.place(x=20, y=85)
+        self.ent_first_name = tk.Entry(self.window, font=('Helvetica', 10, 'bold'))
+        self.ent_first_name.place(x=115, y=85, width=370, height=25)
+        self.ent_first_name.insert(tk.END, data_row.first_name)
+
+        lbl_middle_name = tk.Label(self.window,
+                                   text='Отчество:',
+                                   font=('Helvetica', 10, 'bold')
+                                   )
+        lbl_middle_name.place(x=20, y=120)
+        self.ent_middle_name = tk.Entry(self.window, font=('Helvetica', 10, 'bold'))
+        self.ent_middle_name.place(x=115, y=120, width=370, height=25)
+        self.ent_middle_name.insert(tk.END, data_row.middle_name)
+
+        lbl_last_name = tk.Label(self.window,
+                                   text='Фамилия:',
+                                   font=('Helvetica', 10, 'bold')
+                                   )
+        lbl_last_name.place(x=20, y=155)
+        self.ent_last_name = tk.Entry(self.window, font=('Helvetica', 10, 'bold'))
+        self.ent_last_name.place(x=115, y=155, width=370, height=25)
+        self.ent_last_name.insert(tk.END, data_row.last_name)
+
+        lbl_department = tk.Label(self.window,
+                                   text='Отдел:',
+                                   font=('Helvetica', 10, 'bold')
+                                   )
+        lbl_department.place(x=20, y=190)
+        self.cbo_department = ttk.Combobox(self.window, font=('Helvetica', 10, 'bold'))
+        self.init_combobox()
+        # Запрет на ввод произвольных значений
+        self.cbo_department['state'] = 'readonly'
+        self.set_id_to_combobox(data_row.department_id)
+        self.cbo_department.place(x=115, y=190, width=370, height=25)
+        self.ent_last_name.insert(tk.END, data_row.last_name)
+
+        # Добавление кнопки "Сохранить"
+        self.btn_ok = tk.Button(self.window,
+                                text='Сохранить',
+                                font=('Helvetica', 10, 'bold'),
+                                bg='#ccffcc',
+                                bd=2,
+                                relief='raised',
+                                overrelief='ridge',
+                                activebackground='#345',
+                                activeforeground='white',
+                                command=self.save
+                                )
+        self.btn_ok.place(x=140, y=240, width=90, height=30)
+
+        # Добавление кнопки "Отмена"
+        self.btn_cancel = tk.Button(self.window,
+                                    text='Отмена',
+                                    font=('Helvetica', 10, 'bold'),
+                                    bg='#ffffee',
+                                    bd=2,
+                                    relief='raised',
+                                    overrelief='ridge',
+                                    activebackground='#345',
+                                    activeforeground='white',
+                                    command=self.close
+                                    )
+        self.btn_cancel.place(x=250, y=240, width=90, height=30)
+
+    # Функция заполнения выпадающего списка
+    def init_combobox(self):
+        self.cbox_ids = []
+        self.cbox_values = []
+        data_rows = DepartmentDataHandler.select_list()
+        for data_row in data_rows:
+            self.cbox_ids.append(data_row.department_id)
+            self.cbox_values.append(data_row.department_name)
+        self.cbo_department['values'] = self.cbox_values
+
+    # Функция применения id к выпадающему списку
+    def set_id_to_combobox(self, id):
+        try:
+            id_index = self.cbox_ids.index(int(id))
+            self.cbo_department.set(self.cbox_values[id_index])
+        except ValueError as e:
+            self.cbo_department.set('')
+
+    # Функция считывания id из выпадающего списка
+    def get_id_from_combobox(self, value: str) -> int:
+        try:
+            id_index = self.cbox_values.index(value)
+            return self.cbox_ids[id_index]
+        except ValueError as e:
+            return 0
+        pass
+
+    # Функция открытия окна
+    def open(self):
+        # Перевод фокуса на созданное окно
+        self.window.focus_force()
+        # Перевод всех команд на созданное окно
+        self.window.grab_set()
+
+    # Функция сохранения записи и закрытия окна
+    def save(self):
+        self.collect_from_controls()
+        if self.add_new:
+            self.parent.add_record_callback(self.data_row)
+        else:
+            self.parent.edit_record_callback(self.data_row)
+        self.close()
+
+    # Функция закрытия окна
+    def close(self):
+        self.window.destroy()
+
+    # Функция сбора информации с полей ввода
+    def collect_from_controls(self):
+        self.data_row.first_name = str(self.ent_first_name.get())
+        self.data_row.middle_name = str(self.ent_middle_name.get())
+        self.data_row.last_name = str(self.ent_last_name.get())
+        self.data_row.department_id = str(self.get_id_from_combobox(self.cbo_department.get()))
